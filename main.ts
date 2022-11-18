3a8f4c3dc5be07a09d5095a23ce81a7803992911
emsain01 <117919670+emsain01@users.noreply.github.com>
2022-11-18 08:58:31 -0500

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,83 @@
+basic.showLeds(`
+    . . . . .
+    . # # # .
+    . # # # .
+    . # . . .
+    . # . . .
+    `)
+basic.pause(500)
+basic.showString("the letter p")
+basic.showLeds(`
+    . # # # .
+    # . . . #
+    # . . . #
+    # . . . #
+    . # # # .
+    `)
+basic.pause(500)
+basic.showLeds(`
+    . . # . .
+    . # # # .
+    . . # . .
+    . . # . .
+    . . # . .
+    `)
+basic.pause(500)
+basic.showString("Circle")
+basic.showLeds(`
+    . . # . .
+    . # # # .
+    # # # # #
+    . . # . .
+    . . # . .
+    `)
+basic.pause(500)
+basic.showString("Arrow")
+basic.showLeds(`
+    . . # . .
+    . # # # .
+    # # # # #
+    . # # # .
+    . # # # .
+    `)
+basic.pause(500)
+basic.showString("house ")
+basic.showLeds(`
+    . . # . .
+    . # # # .
+    . # # # .
+    . . # . .
+    . . # . .
+    `)
+basic.pause(500)
+basic.showString("tree")
+basic.showLeds(`
+    # # # # #
+    # . . . .
+    # # # . .
+    # . . . .
+    # # # # #
+    `)
+basic.pause(500)
+basic.showString("the letter E")
+basic.showLeds(`
+    . # . # .
+    . # . # .
+    . . . . .
+    # # . # #
+    . # # # .
+    `)
+basic.pause(500)
+basic.showString("smiley faces ")
+basic.showLeds(`
+    . . . . .
+    . # . # .
+    . . . . .
+    . # # # .
+    # . . . #
+    `)
+basic.pause(500)
+basic.showString("frown ")
+basic.forever(function () {
+	
+})
